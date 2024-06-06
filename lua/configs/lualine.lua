874fc648974c8fda5d9e554be75dad580b9c6814
linulas <linubran90@gmail.com>
2024-06-06 15:07:26 +0200

    bugfix all active buffers gets printed

--- lua/configs/lualine.lua
+++ lua/configs/lualine.lua
@@ -69,9 +69,7 @@ end
 
 -- LSP clients attached to buffer
 local clients_lsp = function()
-  local bufnr = vim.api.nvim_get_current_buf()
-
-  local clients = vim.lsp.get_active_clients { bufnr }
+  local clients = vim.lsp.get_active_clients { bufnr = vim.api.nvim_get_current_buf() }
   if next(clients) == nil then
     return ""
   end
